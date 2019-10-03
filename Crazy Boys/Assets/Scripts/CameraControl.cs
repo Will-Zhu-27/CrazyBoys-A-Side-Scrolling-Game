70389a87d41cb554e28e0d606351725328daf744
Will_Zhu Win Desktop <will_zyq@qq.com>
2019-10-03 19:29:09 +1000

    update jump action

--- Crazy Boys/Assets/Scripts/CameraControl.cs
+++ Crazy Boys/Assets/Scripts/CameraControl.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform lookAtCharacter;
-
-    // Update is called once per frame
-    void Update()
+    public float smoothSpeed = 0.125f;
+    public Vector3 posOffset;
+    void LateUpdate()
     {
         this.transform.LookAt(lookAtCharacter);
+        // Vector3 originPos = this.transform.position;
+        // Vector3 characterPos = lookAtCharacter.position;
+        // this.transform.Translate(new Vector3(characterPos.x - originPos.x, 0, 0));
+        Vector3 desiredPos = lookAtCharacter.position + posOffset;
+        Vector3 smoothedPos = Vector3.Lerp(this.transform.position, desiredPos, smoothSpeed);
+        this.transform.position = smoothedPos;
     }
 }
--- Crazy Boys/Assets/Scripts/CharacterControl.cs
+++ Crazy Boys/Assets/Scripts/CharacterControl.cs
@@ -4,50 +4,77 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    [SerializeField] private string forwardInputName;
+    [SerializeField] private string rightInputName;
     public float walkForwardSpeed;
     public float walkBackwardSpeed;
     public float runSpeed;
-    public KeyCode jumpKeyCode;
-    public KeyCode runKeyCode;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKeyCode;
+    [SerializeField] private KeyCode runKeyCode;
     public Animator animator;
     public CharacterController characterController;
-    private float inputX;
-    private float inputZ;
+    private float forwardInput;
+    private float rightInput;
+    private bool isWalk = false;
+    private bool isJump = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        // Debug.Log(transform.TransformDirection(Vector3.forward).normalized);
     }
-
     // Update is called once per frame
     void Update()
     {
-        inputX = Input.GetAxis("Horizontal");
-        inputZ = Input.GetAxis("Vertical");
-        animator.SetFloat("inputX", inputX);
-        animator.SetFloat("inputZ", inputZ);
-        float moveZ;
-        if (inputZ > 0) {
-            moveZ = inputZ * Time.deltaTime * walkForwardSpeed;
-        } else {
-             moveZ = inputZ * Time.deltaTime * walkBackwardSpeed;
-        }
-        if (Input.GetKey(runKeyCode)) {
-            animator.SetBool("isRun", true);
-        } else {
-            animator.SetBool("isRun", false);
-        }
-        if (Input.GetKey(jumpKeyCode)) {
-            animator.SetBool("isJump", true);
-        } else {
-            animator.SetBool("isJump", false);
-        }
+        forwardInput = Input.GetAxis(forwardInputName);
+        rightInput = Input.GetAxis(rightInputName);
 
-        float moveX = inputX * Time.deltaTime * walkForwardSpeed;
-        
-        Vector3 move = this.transform.forward * moveZ + this.transform.right * moveX;
-        characterController.Move(move);
+        animator.SetFloat("forwardInput", forwardInput);
+        animator.SetFloat("rightInput", rightInput);
 
+        if (forwardInput >= -0.1 && forwardInput <= 0.1) {
+            isWalk = false;  
+        } else {
+            isWalk = true;
+        }
+        animator.SetBool("isWalk", isWalk);
+
+        if (!isJump && Input.GetKey(jumpKeyCode)) {
+            Debug.Log("jump event!");
+            isJump = true;
+            StartCoroutine(JumpEvent());
+        }
+        animator.SetBool("isJump", isJump);
+
+        CharacterMove();
+    }
+
+    private void CharacterMove() {
+        float forwardMovement = 0f, rightMovement = 0f;
+        if (forwardInput > 0.1) {
+            forwardMovement = forwardInput * walkForwardSpeed;
+        } else if (forwardInput < -0.1) {
+            forwardMovement = forwardInput * walkBackwardSpeed;
+        }
+        rightMovement = rightInput * walkForwardSpeed;
+        Vector3 forwardVect = transform.TransformDirection(Vector3.forward).normalized * forwardMovement;
+        Vector3 rightVect = transform.TransformDirection(Vector3.right).normalized * rightMovement;
+        characterController.SimpleMove(forwardVect + rightVect);
+    }
+
+    private IEnumerator JumpEvent() {
+        characterController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            characterController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!characterController.isGrounded && characterController.collisionFlags != CollisionFlags.Above);
+
+        characterController.slopeLimit = 45.0f;
+        isJump = false;
     }
 }
