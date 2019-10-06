8362315f613e8e43a4f6d46f7441ac0ad49e8aba
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-10-06 16:31:03 +1100

    finish Demo2: head watches dynamic point

---
+++ Crazy Boys/Assets/Scripts/Demo2/HandgunShooting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunShooting : MonoBehaviour
+{
+    private Vector3 lookPos;
+    public GameObject aim;
+    private PlayerIKController playerIKController;
+
+    private void Start() {
+        playerIKController = GetComponent<PlayerIKController>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            HandleAimingPos();
+        } else {
+            playerIKController.setIKActive(false);
+        }
+        
+    }
+
+
+    private void HandleAimingPos() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+            Vector3 lookP = hit.point;
+            lookP.z = transform.position.z;
+            lookPos = lookP;
+            aim.transform.position = lookPos;
+            playerIKController.setIKActive(true);
+        } else {
+            playerIKController.setIKActive(false);
+        }
+    }
+
+}
---
+++ Crazy Boys/Assets/Scripts/Demo2/PlayerIKController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class PlayerIKController : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private bool ikActive = false;
+    public Transform lookObj = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK()
+    {
+        if(animator) {
+            
+            //if the IK is active, set the position and rotation directly to the goal. 
+            if(ikActive) {
+
+                // Set the look target position, if one has been assigned
+                if(lookObj != null) {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }    
+
+                // Set the right hand target position and rotation, if one has been assigned
+                // if(rightHandObj != null) {
+                //     animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
+                //     animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);  
+                //     animator.SetIKPosition(AvatarIKGoal.RightHand,rightHandObj.position);
+                //     animator.SetIKRotation(AvatarIKGoal.RightHand,rightHandObj.rotation);
+                // }        
+                
+            }
+            
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else {          
+                // animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
+                // animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0); 
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+
+    public void setIKActive(bool status) {
+        ikActive = status;
+    }    
+}
