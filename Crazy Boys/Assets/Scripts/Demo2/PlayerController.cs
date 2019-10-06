a9f19994ca7ee69534df15d9fa5a019755471a23
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-10-06 13:56:44 +1100

    finish Demo2: add run action

--- Crazy Boys/Assets/Scripts/Demo2/PlayerController.cs
+++ Crazy Boys/Assets/Scripts/Demo2/PlayerController.cs
@@ -8,14 +8,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float forwardWalkSpeed;
     [SerializeField] private float backwardWalkSpeed;
+    [SerializeField] private float crouchingForwardWalkSpeed;
+    [SerializeField] private float crouchingBackwardWalkSpeed;
+    [SerializeField] private float runSpeed;
     [SerializeField] private string forwardMoveInputName = "Horizontal";
     [SerializeField] private KeyCode crouchKeyCode = KeyCode.S;
+    [SerializeField] private KeyCode runKeyCode = KeyCode.LeftShift;
     private CharacterController characterController;
     private Animator animator;
     private float forwardMoveInput;
     private int forwardMoveId;
     private int isCrouchId;
     private bool isCrouch = false;
+    private int isRunId;
+    private bool isRun = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +29,7 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         forwardMoveId = Animator.StringToHash("forwardMove");
         isCrouchId = Animator.StringToHash("isCrouch");
-
+        isRunId = Animator.StringToHash("isRun");
     }
 
     // Update is called once per frame
@@ -32,20 +38,41 @@ public class PlayerController : MonoBehaviour
         forwardMoveInput = Input.GetAxis(forwardMoveInputName);
         animator.SetFloat(forwardMoveId, forwardMoveInput);
 
+        // judge crouch
         if (Input.GetKeyDown(crouchKeyCode)) {
             isCrouch = true;
         } else if (Input.GetKeyUp(crouchKeyCode)) {
             isCrouch = false;
         }
         animator.SetBool(isCrouchId, isCrouch);
+
+        // judge run
+        if (Input.GetKeyDown(runKeyCode)) {
+            isRun = true;
+        } else if (Input.GetKeyUp(runKeyCode)) {
+            isRun = false;
+        }
+        animator.SetBool(isRunId, isRun);
+
         CharacterMove();
     }
         private void CharacterMove() {
         float forwardMovement = 0f;
         if (forwardMoveInput > 0.05f) {
-            forwardMovement = forwardMoveInput * forwardWalkSpeed;
+            if (isRun && !isCrouch) {
+                forwardMovement = forwardMoveInput * runSpeed;
+            } else if (!isCrouch){
+                forwardMovement = forwardMoveInput * forwardWalkSpeed;
+            } else {
+                forwardMovement = forwardMoveInput * crouchingForwardWalkSpeed;
+            }
         } else if (forwardMoveInput < -0.05f) {
-            forwardMovement = forwardMoveInput * backwardWalkSpeed;
+            if (!isCrouch) {
+                forwardMovement = forwardMoveInput * backwardWalkSpeed;
+            } else {
+                forwardMovement = forwardMoveInput * crouchingBackwardWalkSpeed;
+            }
+            
         }
         Vector3 forwardVect = transform.TransformDirection(Vector3.forward).normalized * forwardMovement;
         characterController.SimpleMove(forwardVect);
