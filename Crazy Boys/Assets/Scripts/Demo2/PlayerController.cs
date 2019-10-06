724db9e1372c5fb5a6c42b49ca12a927dac7d933
Will_Zhu Win Desktop <will_zyq@qq.com>
2019-10-07 03:24:08 +1100

    finish Demo2: fix animation rotation bug

--- Crazy Boys/Assets/Scripts/Demo2/PlayerController.cs
+++ Crazy Boys/Assets/Scripts/Demo2/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private string forwardMoveInputName = "Horizontal";
     [SerializeField] private KeyCode crouchKeyCode = KeyCode.S;
     [SerializeField] private KeyCode runKeyCode = KeyCode.LeftShift;
+    [SerializeField] private Transform trackObj;
+    [SerializeField] private float trunBackThreshold = 0f;
+    [SerializeField] private float homingSpeed = 0.125f;
     private CharacterController characterController;
     private Animator animator;
     private float forwardMoveInput;
@@ -22,6 +25,11 @@ public class PlayerController : MonoBehaviour
     private bool isCrouch = false;
     private int isRunId;
     private bool isRun = false;
+    private int isTurnBackId;
+    private bool isTurnBack;
+    private bool isFaceForward;
+    private int isFaceForwardId;
+    private Transform targetObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,12 +38,20 @@ public class PlayerController : MonoBehaviour
         forwardMoveId = Animator.StringToHash("forwardMove");
         isCrouchId = Animator.StringToHash("isCrouch");
         isRunId = Animator.StringToHash("isRun");
+        isTurnBackId = Animator.StringToHash("isTurnBack");
+        isFaceForwardId = Animator.StringToHash("isFaceForward");
+        isFaceForward = animator.GetBool(isFaceForwardId);
+        Debug.Log(transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        isFaceForward = animator.GetBool(isFaceForwardId);
         forwardMoveInput = Input.GetAxis(forwardMoveInputName);
+        if (!isFaceForward) {
+            forwardMoveInput = -forwardMoveInput;
+        }
         animator.SetFloat(forwardMoveId, forwardMoveInput);
 
         // judge crouch
@@ -53,10 +69,21 @@ public class PlayerController : MonoBehaviour
             isRun = false;
         }
         animator.SetBool(isRunId, isRun);
-
+        detectTurnBack();
         CharacterMove();
     }
-        private void CharacterMove() {
+
+    void FixedUpdate() {
+        Quaternion quaternion;
+        if (isFaceForward) {
+            quaternion = new Quaternion(0.0f, 0.7f, 0.0f, 0.7f);
+        } else {
+            quaternion = new Quaternion(0.0f, 0.7f, 0.0f, -0.7f);
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, homingSpeed);
+    }
+
+    private void CharacterMove() {
         float forwardMovement = 0f;
         if (forwardMoveInput > 0.05f) {
             if (isRun && !isCrouch) {
@@ -76,5 +103,14 @@ public class PlayerController : MonoBehaviour
         }
         Vector3 forwardVect = transform.TransformDirection(Vector3.forward).normalized * forwardMovement;
         characterController.SimpleMove(forwardVect);
+    }
+
+    private void detectTurnBack() {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Walking Turn 180") && this.transform.InverseTransformPoint(trackObj.position).z < trunBackThreshold) {
+            isTurnBack = true;
+        } else {
+            isTurnBack = false;
+        }
+        animator.SetBool(isTurnBackId, isTurnBack);
     }
 }
