766d11f5ba7fdcbc249d4da194ce958bcac6f2a6
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-10-06 12:23:49 +1100

    finish Demo2: walk forward and backward

---
+++ Crazy Boys/Assets/Scripts/Demo2/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float forwardWalkSpeed;
+    [SerializeField] private float backwardWalkSpeed;
+    [SerializeField] private string forwardMoveInputName = "Horizontal";
+    private CharacterController characterController;
+    private Animator animator;
+    private float forwardMoveInput;
+    private int forwardMoveId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        forwardMoveId = Animator.StringToHash("forwardMove");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardMoveInput = Input.GetAxis(forwardMoveInputName);
+        animator.SetFloat(forwardMoveId, forwardMoveInput);
+        CharacterMove();
+    }
+        private void CharacterMove() {
+        float forwardMovement = 0f;
+        if (forwardMoveInput > 0.05f) {
+            forwardMovement = forwardMoveInput * forwardWalkSpeed;
+        } else if (forwardMoveInput < -0.05f) {
+            forwardMovement = forwardMoveInput * backwardWalkSpeed;
+        }
+        Vector3 forwardVect = transform.TransformDirection(Vector3.forward).normalized * forwardMovement;
+        characterController.SimpleMove(forwardVect);
+    }
+}
