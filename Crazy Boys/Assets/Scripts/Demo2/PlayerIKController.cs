88047b3497f643302440d6f4a291438ad598fb4c
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-10-09 11:00:37 +1100

    fix: hand rotation

--- Crazy Boys/Assets/Scripts/Demo2/PlayerIKController.cs
+++ Crazy Boys/Assets/Scripts/Demo2/PlayerIKController.cs
@@ -7,12 +7,15 @@ public class PlayerIKController : MonoBehaviour
 {
     private Animator animator;
     public bool ikActive = false;
-    [SerializeField] private Transform lookObj = null;
+    [SerializeField] private Transform lookAim = null;
     public bool isHeadWatch = true;
     public bool isLeftHandToward = true;
+    public float leftHandRotationWeight = 0f;
+    public Transform leftHandAim;
     public bool isRightHandToward = true;
-    public float rightHandRotationWeight;
-    public Transform rightHandObj;
+    public float rightHandRotationWeight = 0f;
+
+    public Transform rightHandAim;
     
     // Start is called before the first frame update
     void Start()
@@ -27,23 +30,36 @@ public class PlayerIKController : MonoBehaviour
             if (isHeadWatch)
             {
                 animator.SetLookAtWeight(1);
-                animator.SetLookAtPosition(lookObj.position);
+                animator.SetLookAtPosition(lookAim.position);
             }
             else
             {
                 animator.SetLookAtWeight(0);
             }
-            if (isRightHandToward && !animator.GetCurrentAnimatorStateInfo(0).IsName("Walking Turn 180")) {
+
+            if (isLeftHandToward) {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandAim.position);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand,leftHandRotationWeight);  
+            } else {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand,0);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+            }
+
+            if (isRightHandToward) {
                 animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-                animator.SetIKPosition(AvatarIKGoal.RightHand, lookObj.position);
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandAim.position);
                 animator.SetIKRotationWeight(AvatarIKGoal.RightHand,rightHandRotationWeight);              
             } else {
                 animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
                 animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
             }
 
+
         } else {
             animator.SetLookAtWeight(0);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand,0);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
             animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
             animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
         }
--- Crazy Boys/Assets/Scripts/Demo2/TowardsMouse.cs
+++ Crazy Boys/Assets/Scripts/Demo2/TowardsMouse.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class TowardsMouse : MonoBehaviour
 {
-    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform eyePoint;
     [SerializeField] private Transform watchPoint;
+    [SerializeField] private Transform leftShoulder;
+    [SerializeField] private Transform leftShoulderWatchPoint;
+    [SerializeField] private float leftShoulderWatchPointFactor;
     [SerializeField] private Transform rightShoulder;
     [SerializeField] private Transform rightShoulderWatchPoint;
     [SerializeField] private float rightShoulderWatchPointFactor;
@@ -16,22 +19,43 @@ public class TowardsMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        screenPostion = Camera.main.WorldToScreenPoint(startPoint.position);
         mousePositionOnScreen = Input.mousePosition;
-        mousePositionOnScreen.z = screenPostion.z;
-        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
-        watchPoint.position = mousePositionInWorld;
+        // eye point
+        if (eyePoint && watchPoint) {
+            screenPostion = Camera.main.WorldToScreenPoint(eyePoint.position);
+            mousePositionOnScreen.z = screenPostion.z;
+            mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+            watchPoint.position = mousePositionInWorld;
+        }
+       
 
-        screenPostion = Camera.main.WorldToScreenPoint(rightShoulder.position);
-        mousePositionOnScreen = Input.mousePosition;
-        mousePositionOnScreen.z = screenPostion.z;
-        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
-        rightShoulderWatchPoint.position = mousePositionInWorld;
-        HandleRightShoulderWatchPoint();
+        //left hand point
+        if (leftShoulder && leftShoulderWatchPoint) {
+            screenPostion = Camera.main.WorldToScreenPoint(leftShoulder.position);
+            mousePositionOnScreen.z = screenPostion.z;
+            mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+            leftShoulderWatchPoint.position = mousePositionInWorld;
+            // HandleRightShoulderWatchPoint();
+            HandleWatchPoint(leftShoulder, leftShoulderWatchPoint, leftShoulderWatchPointFactor);  
+        }
+
+        // right hand point
+        if (rightShoulder && rightShoulderWatchPoint) {
+            screenPostion = Camera.main.WorldToScreenPoint(rightShoulder.position);
+            mousePositionOnScreen.z = screenPostion.z;
+            mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+            rightShoulderWatchPoint.position = mousePositionInWorld;
+            HandleWatchPoint(rightShoulder, rightShoulderWatchPoint, rightShoulderWatchPointFactor);  
+        }
     }
 
     void HandleRightShoulderWatchPoint() {
         Vector3 direction = (rightShoulderWatchPoint.position - rightShoulder.position).normalized;
         rightShoulderWatchPoint.position = rightShoulder.position + direction * rightShoulderWatchPointFactor;
+    }
+
+    void HandleWatchPoint(Transform start, Transform end, float factor) {
+        Vector3 direction = (end.position - start.position).normalized;
+        end.position = start.position + direction * factor;
     }
 }
