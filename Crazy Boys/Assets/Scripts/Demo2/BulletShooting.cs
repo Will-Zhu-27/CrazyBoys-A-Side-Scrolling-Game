e5ecdedc86577dbd76a80932387bac013636fa09
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-10-15 16:16:41 +1100

    now player can shoot bullet and bullet will be destroeyed when it is out of view

--- Crazy Boys/Assets/Scripts/Demo2/BulletShooting.cs
+++ Crazy Boys/Assets/Scripts/Demo2/BulletShooting.cs
@@ -6,16 +6,28 @@ public class BulletShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public Transform bulletAim;
     [SerializeField] private KeyCode shootingKeyCode = KeyCode.Mouse0 ;
     [SerializeField] private float bulletSpeed = 15f;
+    [SerializeField] private Vector3 bulletRotationOffset = Vector3.zero;
+
+
     // Start is called before the first frame update
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(shootingKeyCode)) {
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.VelocityChange);
+            Vector3 rotation = bulletSpawn.rotation.eulerAngles + bulletRotationOffset;
+            Quaternion quaternion = Quaternion.Euler(rotation);
+
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, quaternion);
+            // bullet.transform.LookAt(bulletAim);
+            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+
+            bulletRigidbody.AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.Impulse);
+            // Physics.IgnoreCollision(bulletRigidbody, bulletSpawn.GetComponent)
         }
     }
 }
