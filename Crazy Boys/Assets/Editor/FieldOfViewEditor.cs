7ce98b58dbd91cb87f0ad76146093489221e418c
BobyZhang <zsb0213@gmail.com>
2019-10-16 22:25:37 +1100

    enemy view

---
+++ Crazy Boys/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.enemyEyesPosition, Vector3.forward, Vector3.left, 360f, fow.viewRadius);
+
+        if (fow.targetPlayer != null)
+        {
+            Handles.color = Color.red;
+
+
+            Handles.DrawLine(fow.enemyEyesPosition, fow.playerHeadPosition);
+        }
+        
+    }
+    
+}
