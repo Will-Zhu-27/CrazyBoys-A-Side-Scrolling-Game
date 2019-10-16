3f13ff215e7bfef6ae7d29ed110d96747f445a49
Will_Zhu Win Desktop <will_zyq@qq.com>
2019-10-17 08:33:23 +1100

    add audio material and set handgun shooting and reloading sound

--- Crazy Boys/Assets/Scripts/Demo2/BulletShooting.cs
+++ Crazy Boys/Assets/Scripts/Demo2/BulletShooting.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class BulletShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
+
     public Transform rightBulletSpawn;
     public Transform rightBulletRotation;
     public Transform leftBulletSpawn;
     [SerializeField] private KeyCode shootingKeyCode = KeyCode.Mouse0 ;
-    [SerializeField] private float bulletSpeed = 10f;
+    public AudioClip handgunShoot;
+    [SerializeField] private KeyCode reloadingKeyCode = KeyCode.R ;
+    public AudioClip handgunReload;
+    [SerializeField] private float bulletSpeed = 15f;
     [SerializeField] private Vector3 bulletRotationOffset = Vector3.zero;
     private bool isRightShooting = true;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
-
+    void Start() {
+        audioSource = this.GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(shootingKeyCode)) {
+            audioSource.clip = handgunShoot;
+            audioSource.Play();
             Transform start = null;
             if (isRightShooting) {
                 start = rightBulletSpawn;
@@ -33,6 +43,12 @@ public class BulletShooting : MonoBehaviour
 
             bulletRigidbody.AddForce(start.forward * bulletSpeed, ForceMode.Impulse);
             isRightShooting = !isRightShooting;
+        }
+        if (Input.GetKeyDown(reloadingKeyCode)) {
+            if (!audioSource.isPlaying) {
+                audioSource.clip = handgunReload;
+                audioSource.Play();
+            }
         }
     }
 }
