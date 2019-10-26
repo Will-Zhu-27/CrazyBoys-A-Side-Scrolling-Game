691a352c7e1549d885f2a837ee67239992b9c544
Will_Zhu <yuqiangz@student.unimelb.edu.au>
2019-10-26 12:11:30 +1100

    main menu

---
+++ Crazy Boys/Assets/Scripts/SceneControllers/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    
+    public void StartGameLevelEasy()
+    {
+        SceneManager.LoadScene("Indoor Demo");
+    }
+
+    public void StartGameLevelHard()
+    {
+        SceneManager.LoadScene("Indoor");
+    }
+
+    public void GoToInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
