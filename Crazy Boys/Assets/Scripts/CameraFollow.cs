39473c6e159bcf1aa013dee0a1a8d0838262bcca
Will_Zhu <yuqiangz@student.unimelb.edu.au>
2019-10-26 23:08:22 +1100

    Perfect and delete unused file to submit (#11)

---
+++ Crazy Boys/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smothing = 10f;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+    }
+    // Update is called once per frame
+    void LateUpdate () {
+        Vector3 targetCampos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCampos, smothing * Time.deltaTime);
+    }
+
+}
